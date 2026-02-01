4fdf00a21e035c4849f0c91a31e71723b1d6f556
cristiandias504 <cristiandias504@gmail.com>
2026-02-01 09:44:57 -0500

    Se crea la primera comunicación Bluetooth BLE entre el sistema electrónico y la aplicación movil.

--- ProyectoLlaveArduino.ino
+++ ProyectoLlaveArduino.ino
@@ -1,6 +1,81 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
 
+
+#define SERVICE_UUID        "12345678-1234-1234-1234-1234567890ab"
+#define CHARACTERISTIC_RX  "12345678-1234-1234-1234-1234567890ac"
+#define CHARACTERISTIC_TX  "12345678-1234-1234-1234-1234567890ad"
+
+BLECharacteristic *txCharacteristic;
+
+String mensajeRecibido = ""; // buffer global
+
+
+class ServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    Serial.println("✅ Cliente conectado");
+  }
+
+  void onDisconnect(BLEServer* pServer) {
+    Serial.println("❌ Cliente desconectado");
+    pServer->startAdvertising();
+  }
+};
+
+
+class RecibirMensajeBLE : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic) {
+    mensajeRecibido = pCharacteristic->getValue();
+    Serial.println(mensajeRecibido);
+  }
+};
+
+
+void EnviarMensajeBLE(String mensaje) {
+    txCharacteristic->setValue(mensaje);
+    txCharacteristic->notify();
+    Serial.print("📤 Mensaje enviado: ");
+    Serial.println(mensaje);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println("Iniciando BLE...");
+  IniciarBLE();
+
+}
+
+void IniciarBLE() {
+  BLEDevice::init("ESP32_BLE_TEST");
+
+  BLEServer *server = BLEDevice::createServer();
+  server->setCallbacks(new ServerCallbacks());
+
+  BLEService *service = server->createService(SERVICE_UUID);
+
+  // Característica TX (ESP32 → cliente)
+  txCharacteristic = service->createCharacteristic(
+    CHARACTERISTIC_TX,
+    BLECharacteristic::PROPERTY_NOTIFY);
+  txCharacteristic->addDescriptor(new BLE2902());
+
+  // Característica RX (cliente → ESP32)
+  BLECharacteristic *rxCharacteristic = service->createCharacteristic(
+    CHARACTERISTIC_RX,
+    BLECharacteristic::PROPERTY_WRITE);
+  rxCharacteristic->setCallbacks(new RecibirMensajeBLE());
+
+  service->start();
+
+  BLEAdvertising *advertising = BLEDevice::getAdvertising();
+  advertising->addServiceUUID(SERVICE_UUID);
+  advertising->setScanResponse(true);
+  advertising->start();
+
+  Serial.println("📡 Advertencia BLE iniciada");
 }
 
 void loop() {
